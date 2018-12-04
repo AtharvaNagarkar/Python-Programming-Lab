2b895bdb09d00da478662fb00d36af21c4483fa4
AtharvaNagarkar <43986130+AtharvaNagarkar@users.noreply.github.com>
2018-12-04 08:56:39 +0530

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -1,13 +1,13 @@
-def Arm(num):
-	sum = 0
-	temp = num
-	while temp > 0:
-   		digit = temp % 10
-   		sum += digit ** 3
-   		temp //= 10
-	if num == sum:
+//Atharva Nagarkar M 42
+def Arm(num):							#define function	
+	sum = 0							#initialize variables
+	temp = num					
+	while temp > 0:						#while loop		
+   		digit = temp % 10				#remainder of number with 10
+   		sum += digit ** 3				#update sum by cube of number	
+   		temp //= 10					#update number by quotient with 10
+	if num == sum:						#compare old and new numbers
 	        print(num,"is an Armstrong number")
-	else:
+	else:	
    		print(num,"is not an Armstrong number")
-num = int(input("Enter a number: "))
-Arm(num)
+num = int(input("Enter a number: "))				#input from user
