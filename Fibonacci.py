d2a316c5b13073939278c0fada9a0b12272b1ef6
AtharvaNagarkar <43986130+AtharvaNagarkar@users.noreply.github.com>
2018-12-04 08:50:14 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,19 +1,20 @@
-def fib(x):
-	l=[0,1]
+//Atharva Nagarkar M 42
+def fib(x):							#define funvtion
+	l=[0,1]							#innitialize variables ani list
 	a=0
 	b=1
-	if x<0:
+	if x<0:							#negative number entered
 		print("Enter a positive number ")
-	elif x==0:
+	elif x==0:						#if number is x=0
 		print([0])
-	elif x==1:
+	elif x==1:						#if number is x=1
 		print([0,1])
-	else:
-		for i in range (x-2):
-			f=a+b
-			l.append(f)
-			a=b
+	else:							#for some positive number
+		for i in range (x-2):				#for number til x-2
+			f=a+b					#add 2 numbers
+			l.append(f)				#append sum to list
+			a=b					#assign new values to variables
 			b=f
-	return l
-x=int(input("Enter number of terms : "))
-print(fib(x))
+	return l						#return list
+x=int(input("Enter number of terms : "))			#input from user
+print(fib(x))							#print output
