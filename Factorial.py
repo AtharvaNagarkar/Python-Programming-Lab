859ce5d11aec1081c48197b2e568f5863d2de1a0
AtharvaNagarkar <43986130+AtharvaNagarkar@users.noreply.github.com>
2018-12-04 08:53:08 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,15 +1,16 @@
-def fact(x):
-	f=1
-	if x<0:
+//Atharva Nagarkar M 42
+def fact(x):						#define function
+	f=1						#intialize f
+	if x<0:						#negative number enter
 		print("Enter a positive number")
-	elif x==0:
+	elif x==0:					#number=0
 		print("Factorial : 1")
-	elif x==1:
+	elif x==1:	`				#number=1
 		print("Factorial : 1")
-	else:
-		while x>1:
-			f=f*x
-			x=x-1
-	return f
-x=int(input("Enter a number : "))
-print(fact(x))
+	else:						#some positive number
+		while x>1:				#while loop
+			f=f*x				#update f
+			x=x-1				#decrease counter
+	return f					#return factorial
+x=int(input("Enter a number : "))			#input from user 
+print(fact(x))						#print factorial
